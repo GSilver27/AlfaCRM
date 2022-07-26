a9339ed68704537fab06485bab5ca3605f2fe187
Silver <gramakov27@gmail.com>
2022-07-27 00:47:53 +0300

    о учителе

--- frontend/views/teacher/index.php
+++ frontend/views/teacher/index.php
@@ -21,20 +21,44 @@ $this->title = 'Мой кабинет';
     </div>
 
     <div class="teacher_main">
-        <div>
-            <div class="teacher_info">
+        <div class="teacher_main-info">
+            <div class="teacher_photo">
                 <div>
-                    <img class="teacher_photo" src="<?= Url::to(['img/Ellipse 5.png']) ?>" alt="Фото учителя">
+                    <img class="teacher_photo-img" src="<?= Url::to(['img/Ellipse 5.png']) ?>" alt="Фото учителя">
                 </div>
                 <div class="teacher_photo-change">
                     Сменить фото
                 </div>
             </div>
 
+            <div class="teacher_info">
+                <div class="teacher_info-name">Марина Любимова</div>
+                <div class="teacher_info-general">
+                    <div class="teacher_age">17.09.1995</div>
+                    <img class="gender-female" src="<?= Url::to(['img/icons/gender_female.webp']) ?>" alt="женщина">
+                    <div class="teacher_gender">женщина</div>
+                </div>
+                <div class="teacher_info-divider"></div>
+                <div class="teacher_info-email">
+                    <img class="email-img" src="<?= Url::to(['img/icons/email.webp']) ?>" alt="email">
+                    <a href="mailto:flexander.grid@gmail.com">flexander.grid@gmail.com</a>
+                </div>
+            </div>
+
+            <div class="teacher_planning">
+                <div class="teacher_planning-id">
+                    ID <span>#20</span>
+                </div>
+                <div class="teacher_planning-lessons">
+                    Уроки <span>0 штук</span>
+                    <button class="teacher_planning-btn">Запланировать</button>
+                </div>
+            </div>
         </div>
         
-        <div class="">
+        <div class="teacher_schedule">
 
         </div>
+
     </div>
 </main>
